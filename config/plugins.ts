ce6d1b308d216fd17d3bd511f641ad6679d544ed
asad-tech-dev <sidtechdevpk@gmail.com>
2025-07-17 20:08:38 +0500

    Commented other plugins

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,16 +3,16 @@ export default () => ({
     enabled: true,
     resolve: './src/plugins/standard-strapi-toolkit',
   },
-  'content-publisher': {
-    enabled: true,
-    resolve: './src/plugins/content-publisher'
-  },
-  'custom-fields': {
-    enabled: true,
-    resolve: './src/plugins/custom-fields'
-  },
-  'color-picker': {
-    enabled: true,
-    resolve: './src/plugins/color-picker'
-  },
+  // 'content-publisher': {
+  //   enabled: true,
+  //   resolve: './src/plugins/content-publisher'
+  // },
+  // 'custom-fields': {
+  //   enabled: true,
+  //   resolve: './src/plugins/custom-fields'
+  // },
+  // 'color-picker': {
+  //   enabled: true,
+  //   resolve: './src/plugins/color-picker'
+  // },
 });

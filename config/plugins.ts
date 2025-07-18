6b9438434474e054d5fb0a05f0aedfb1dbd5808f
asad-tech-dev <sidtechdevpk@gmail.com>
2025-07-18 12:14:37 +0500

    Commented other unused plugins

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,7 +1,7 @@
 export default () => ({
   'standard-strapi-toolkit': {
     enabled: true,
-    resolve: './src/plugins/standard-strapi-toolkit',
+    resolve: './src/plugins/standard-strapi-toolkit'
   },
   // 'content-publisher': {
   //   enabled: true,

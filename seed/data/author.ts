7690cab55940136dded9c172f4dbb2df0f5c062d
asad-tech-dev <sidtechdevpk@gmail.com>
2025-07-21 16:27:11 +0500

    Added authors data with images

---
+++ seed/data/author.ts
@@ -0,0 +1,7 @@
+import { Author } from "../../types/author";
+
+export default [
+  { name: "Alex Thompson", avatar: "aaron-avatar.png" },
+  { name: "Marcus Rodriguez", avatar: "aaron-avatar.png" },
+  { name: "Sarah Chen", avatar: "samantha-avatar.png" },
+] as Author[];

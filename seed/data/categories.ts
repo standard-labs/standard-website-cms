4e564a14f95cd55e2a7662c6fde393c13e320ee6
asad-tech-dev <sidtechdevpk@gmail.com>
2025-07-22 13:19:39 +0500

    Added Categories data

---
+++ seed/data/categories.ts
@@ -0,0 +1,25 @@
+import { Category } from "../../types/category";
+
+export default [
+  {
+    id: "",
+    name: "Technology",
+    slug: "technology",
+    description: "Exploring the latest advancements in technology, from AI to blockchain and beyond.",
+    articles: [],
+  },
+  {
+    id: "",
+    name: "Development",
+    slug: "development",
+    description: "Insights into software development practices, programming languages, and tools that shape the industry.",
+    articles: [],
+  },
+  {
+    id: "",
+    name: "Management",
+    slug: "management",
+    description: "Strategies and best practices for effective team and project management in the tech industry.",
+    articles: [],
+  },
+] as Category[];

1ee65af447283a97c8758ebe22f4cfc31efeb242
asad-tech-dev <sidtechdevpk@gmail.com>
2025-07-23 15:24:31 +0500

    Reinstalled and fs-extra and mime-types package

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,8 @@
 # npm install
 
 echo "🔧 Building Plugin..."
-npm install --prefix ./src/plugins/standard-strapi-toolkit
-npx strapi-plugin build --prefix ./src/plugins/standard-strapi-toolkit
+# npm install --prefix ./src/plugins/standard-strapi-toolkit
+# npx strapi-plugin build --prefix ./src/plugins/standard-strapi-toolkit
 
 echo "🏗️  Building Strapi..."
 npx strapi build

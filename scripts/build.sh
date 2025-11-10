6137d26dc09797bb95119483a23fe6dbcc1b757b
asad-tech-dev <sidtechdevpk@gmail.com>
2025-11-10 14:41:26 +0500

    Updated build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# echo "🔧 Installing dependencies..."
-# npm install
+echo "🔧 Installing dependencies..."
+npm install
 
-# echo "🔧 Building Plugin..."
-# npm install --prefix ./src/plugins/standard-strapi-toolkit
-# npm run build --prefix ./src/plugins/standard-strapi-toolkit
+echo "🔧 Building Plugin..."
+npm install --prefix ./src/plugins/standard-strapi-toolkit
+npm run build --prefix ./src/plugins/standard-strapi-toolkit
 
 echo "🏗️  Building Strapi..."
 npx strapi build

e9e399e520ef1e9fafc67809d98999b87ebc8d07
asad-tech-dev <sidtechdevpk@gmail.com>
2025-11-10 17:58:30 +0500

    Commented strapi build commands from build.sh file

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "🔧 Installing dependencies..."
-npm install
+# echo "🔧 Installing dependencies..."
+# npm install
 
-echo "🔧 Building Plugin..."
-npm install --prefix ./src/plugins/standard-strapi-toolkit
-npm run build --prefix ./src/plugins/standard-strapi-toolkit
+# echo "🔧 Building Plugin..."
+# npm install --prefix ./src/plugins/standard-strapi-toolkit
+# npm run build --prefix ./src/plugins/standard-strapi-toolkit
 
-echo "🏗️  Building Strapi..."
-npx strapi build
+# echo "🏗️  Building Strapi..."
+# npx strapi build
 
 # npm i lodash
 

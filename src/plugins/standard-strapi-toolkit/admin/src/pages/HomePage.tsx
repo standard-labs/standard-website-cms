45c79693101032c7839ed6c1919c884830ebb0af
asad-tech-dev <sidtechdevpk@gmail.com>
2025-11-12 19:53:28 +0500

    Added "dist" in .gitignore

--- src/plugins/standard-strapi-toolkit/admin/src/pages/HomePage.tsx
+++ src/plugins/standard-strapi-toolkit/admin/src/pages/HomePage.tsx
@@ -2,15 +2,7 @@ import { Main } from '@strapi/design-system';
 import { useIntl } from 'react-intl';
 import { getTranslation } from '../utils/getTranslation';
 
-/**
- * HomePage Component
- *
- * This is the main landing page of the plugin.
- * 
- * This plugin is developed to add **custom functionalities** 
- * to your Strapi CMS project, extending its capabilities 
- * without modifying the core CMS.
- */
+
 const HomePage = () => {
   const { formatMessage } = useIntl();
 

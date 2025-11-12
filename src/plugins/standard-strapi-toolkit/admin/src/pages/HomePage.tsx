46ad1b4e2c8da0d8ec6aa5cfae2400787dbca5de
asad-tech-dev <sidtechdevpk@gmail.com>
2025-11-12 19:41:54 +0500

    Updated detail page of standard-strapi-toolkit plugin to test the deployment

--- src/plugins/standard-strapi-toolkit/admin/src/pages/HomePage.tsx
+++ src/plugins/standard-strapi-toolkit/admin/src/pages/HomePage.tsx
@@ -1,14 +1,27 @@
 import { Main } from '@strapi/design-system';
 import { useIntl } from 'react-intl';
-
 import { getTranslation } from '../utils/getTranslation';
 
+/**
+ * HomePage Component
+ *
+ * This is the main landing page of the plugin.
+ * 
+ * This plugin is developed to add **custom functionalities** 
+ * to your Strapi CMS project, extending its capabilities 
+ * without modifying the core CMS.
+ */
 const HomePage = () => {
   const { formatMessage } = useIntl();
 
   return (
-    <Main>
-      <h1>Welcome to {formatMessage({ id: getTranslation('plugin.name') })}</h1>
+    <Main padding={6}>
+      <h1>
+        Welcome to {formatMessage({ id: getTranslation('plugin.name') })}
+      </h1>
+      <p>
+        This plugin provides additional features to enhance your CMS experience.
+      </p>
     </Main>
   );
 };

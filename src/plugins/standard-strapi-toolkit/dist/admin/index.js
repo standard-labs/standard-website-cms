a72f6f17a408decdce0330b5d8d842c7e21c5042
asad-tech-dev <sidtechdevpk@gmail.com>
2025-11-13 18:12:20 +0500

    Revert "Added plugin build in the git + removed from workflow"
    This reverts commit ea1c154f75c80c633fa85a981ac522818150cf94.

--- src/plugins/standard-strapi-toolkit/dist/admin/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const index = require("../_chunks/index-BlhESxgS.js");
-module.exports = index.index;
-//# sourceMappingURL=index.js.map
--- src/plugins/standard-strapi-toolkit/dist/admin/index.mjs
+++
@@ -1,5 +0,0 @@
-import { i } from "../_chunks/index-D62VBpN8.mjs";
-export {
-  i as default
-};
-//# sourceMappingURL=index.mjs.map
